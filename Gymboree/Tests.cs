1c973fa2b690ab28fa959e3c72e1b5c0200fa2e6
JuneCho23 <junecho.ucb@gmail.com>
2017-07-13 10:14:52 -0700

    cleanup

--- Gymboree/Tests.cs
+++ Gymboree/Tests.cs
@@ -39,68 +39,39 @@ namespace Gymboree
             //The following command will reveal the URL for this app
             //app.Query(x => x.WebView().Invoke("getUrl"))
 
-			Thread.Sleep(8000);
 			app.WaitForElement(x => x.Css("#search"), timeout: TimeSpan.FromSeconds(80));
 			app.Tap(x => x.Css("#search"));
-			Thread.Sleep(8000);
 			app.Screenshot("Tapped on Search Text Field");
 
-			Thread.Sleep(8000);
 			app.EnterText("Microsoft");
-			Thread.Sleep(8000);
 			app.Screenshot("Typed in my Search, 'Microsoft'");
 
 			app.DismissKeyboard();
 			app.Screenshot("Dismissed Keyboard");
 
-			Thread.Sleep(8000);
 			app.Tap(x => x.Css(".icon-search"));
-			Thread.Sleep(8000);
 			app.Screenshot("Tapped on Seach Button");
 
-			Thread.Sleep(8000);
 			app.Tap(x => x.Css("#search"));
-			Thread.Sleep(8000);
 			app.Screenshot("Tapped on Search Text Field");
 
-			Thread.Sleep(8000);
 			app.EnterText("pants");
-			Thread.Sleep(8000);
 			app.Screenshot("Typed in my Search, 'pants'");
 
-			Thread.Sleep(8000);
 			app.Tap(x => x.Css(".icon-search"));
-			Thread.Sleep(8000);
 			app.Screenshot("Tapped on Seach Button");
 
-			Thread.Sleep(8000);
 			app.ScrollDown();
 			app.Screenshot("Scrolling down for more information");
 
-			Thread.Sleep(8000);
 			app.Tap(x => x.Css(".sli_grid_image"));
-			Thread.Sleep(8000);
 			app.Screenshot("Tapped on my pant selection");
 
-			Thread.Sleep(4000);
 			app.Tap(x => x.Css(".icon-shop"));
-			Thread.Sleep(8000);
 			app.Screenshot("Tapped on Hamburger Menu Button");
 
-			Thread.Sleep(8000);
 			app.Tap(x => x.Css("#gbl-kid-girl"));
-			Thread.Sleep(8000);
 			app.Screenshot("Tapped on the catagory, 'Girl");
-
-			Thread.Sleep(8000);
-			app.ScrollDown();
-			Thread.Sleep(8000);
-			app.Screenshot("Scrolling down for more information");
-
 		}
-
-
-
-
 	}
 }
